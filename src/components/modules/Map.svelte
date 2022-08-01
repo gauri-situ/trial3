a327819704edecbd9414365a021a5f4f77fd643a
gauri-situ <105880481+gauri-situ@users.noreply.github.com>
2022-08-01 16:17:48 -0400

    Update Map.svelte

--- src/components/modules/Map.svelte
+++ src/components/modules/Map.svelte
@@ -44,7 +44,7 @@
       bind:this={mapComponent}
       accessToken={MAPBOX_ACCESS_TOKEN}
       options={{
-        style: "mapbox://styles/situmapping/cl27sefh1000r15o2yvb5er0k",
+        style: ""mapbox://styles/situmapping/ckwwkmecf04xr14pfvdyrr9cg",
         center: [
           parseFloat($platform_config_store["Map start longitude"]),
           parseFloat($platform_config_store["Map start latitude"]),
